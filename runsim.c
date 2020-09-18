38b9b5fab278e1532c0e5350e47e4363158f220d
Michael-Dyer <Dyerm636@yahoo.com>
2020-09-18 17:59:46 -0500

    getopt working

--- runsim.c
+++ runsim.c
@@ -3,12 +3,23 @@
 #include <err.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
+
 
 int main (int argc, char* argv[]){
-	int pr_limit;
-	int pr_count = 0;
+	int pr_limit; //max children proccesse
+	int pr_count = 0; //current children
+	int opt;
+
+	pid_t childpid = 0;
+
+	while((opt = getopt(argc, argv, "t")) != -1)
+	switch (opt) {
+		case 't':
+			printf("you entered t");
 
 
+	}	
 
 return 0;
 }
